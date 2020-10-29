b18ac4ccd14b728d3625e9a01212c697636d5813
adriaanboon <71772080+adriaanboon@users.noreply.github.com>
2020-10-29 11:52:25 +0100

    Opdracht 15, eerste versie.

--- src/FilesInlezen.java
+++ src/FilesInlezen.java
@@ -1,3 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FilesInlezen {
+    public static void main(String[] args) {
+        try {
+            File f = new File( "Hello.txt");
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                System.out.println(s.hasNextLine());
+            }
+        } catch (FileNotFoundException f) {
+            System.out.println("Bestand niet gevonden.");
+        }
+    }
 }
--- src/FilesInlezen2.java
+++ src/FilesInlezen2.java
@@ -1,4 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FilesInlezen2 {
 
+        public static void main(String[] args) {
+            try { bestandInlezen();
+            } catch (FileNotFoundException f) {
+                System.out.println("Bestand niet gevonden.");
+            }
+            finally {
+                System.out.println("Einde programma.");
+            }
+        }
+
+    private static void bestandInlezen() throws FileNotFoundException{
+        File f = new File( "Hello.txt");
+        Scanner s = new Scanner(f);
+        while (s.hasNextLine()) {
+            System.out.println(s.hasNextLine());
+
+        }
+    }
 }
